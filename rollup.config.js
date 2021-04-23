ff4eb2e20552bc9cf83e98fcd2a987f3f9628bcb
Raghav Misra <raghav.m2014@gmail.com>
2021-04-23 15:56:14 -0700

    Set up bundling and library targets

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import tsc from "typescript";
+
+const input = "src/index.ts";
+
+const typescriptPlugin = typescript({
+    typescript: tsc,
+});
+
+const terserPlugin = terser();
+
+export default [
+    {
+        input,
+        output: {
+            file: "dist/index.esm.js",
+            format: "esm",
+            sourcemap: true,
+        },
+        plugins: [typescriptPlugin],
+    },
+    {
+        input,
+        output: {
+            file: "dist/index.cjs",
+            format: "cjs",
+            sourcemap: true,
+        },
+        plugins: [typescriptPlugin],
+    },
+    {
+        input,
+        output: {
+            file: "dist/index.js",
+            globals: {
+                "react": "React"
+            }
+        },
+        plugins: [typescriptPlugin, terserPlugin],
+    },
+];
