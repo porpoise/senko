08298a515ccbb271df89354b0987b5a8a707a10b
Raghav Misra <raghav.m2014@gmail.com>
2021-04-23 23:13:08 -0700

    Add checkbox support

--- src/model.ts
+++ src/model.ts
@@ -3,16 +3,26 @@ import React from "react";
 export interface IModel<Value> {
     value: Value;
     onInput: React.FormEventHandler<HTMLInputElement>;
+    onChange: React.FormEventHandler<HTMLInputElement>;
 }
 
 export default function model<Store>(
     store: Store,
     prop: keyof Store
-): IModel<Store[keyof Store]> {
+): IModel<Store[keyof Store]> { 
+    const handler: React.FormEventHandler<HTMLInputElement> = ({ target }) => {
+        let inputProp: "value" | "checked" = "value";
+
+        if ((target as HTMLInputElement).type = "checkbox") {
+            inputProp = "checked";
+        }
+
+        (store[prop] = ((target as HTMLInputElement)[inputProp] as unknown) as Store[keyof Store])
+    }
+
     return {
         value: store[prop],
-        onInput: ({ target }) =>
-            (store[prop] = ((target as HTMLInputElement)
-                .value as unknown) as Store[keyof Store]),
+        onInput: handler,
+        onChange: handler
     };
 }
